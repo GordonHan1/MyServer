f19ace716e582242806384dc84b740e38b1f84ad
Gordon Han <gzhe25@gmail.com>
2025-01-18 17:50:53 +0800

    i hope this works???
    i changed it to colorscheme dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import '@mantine/core/styles.css';
 import { NavbarHeader } from './components/Navbar/navbar';
-
-
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles>
       <NavbarHeader /> {/* Add navbarHeader here */}
       <Router />
     </MantineProvider>
